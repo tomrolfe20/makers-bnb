b45d705f98a872fee3964c6345fc18ea1caa9188
ShaunFlood <shaunflood@hotmail.co.uk>
2023-01-17 17:35:16 +0000

    Seeds complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,62 +2,52 @@ if ENV['RACK_ENV'] == 'test'
   User.create(
     user_name: "name",
     email: "name@example.email",
-    password: "pass123"
+    password_digest: "pass123"
   )
   User.create(
     user_name: "name2",
     email: "name2@example.email",
-    password: "pass123"
+    password_digest: "pass123"
   )
   User.create(
     user_name: "name3",
     email: "name3@example.email",
-    password: "pass123"
-  )
-  User.create(
-    user_name: "name4",
-    email: "name4@example.email",
-    password: "pass123"
-  )
-  User.create(
-    user_name: "name5",
-    email: "name5@example.email",
-    password: "pass123"
+    password_digest: "pass123"
   )
   Space.create(
     name: "Hotel Makers",
     description: "Ikea",
     price_per_night: 200,
-    host_name: "Makers"
+    user_id: 1
   )
   Space.create(
     name: "Hotel Hilton",
     description: "Fancy",
     price_per_night: 1000,
-    host_name: "Hilton"
+    user_id: 2
   )
   Space.create(
     name: "Holiday inn",
     description: "just a bed",
     price_per_night: 100,
-    host_name: "Company Holiday"
-  )
-  Space.create(
-    name: "Hotel I",
-    description: "Bog standard hotel",
-    price_per_night: 150,
-    host_name: "Company I"
+    user_id: 3
   )
   Booking.create(
     date: "2023-01-01",
-    available: true
+    available: false,
+    space_id: 1,
+    user_id: 1
   )
   Booking.create(
-    date: "2023-02-14",
-    available: true
+    date: "2023-01-01",
+    available: true,
+    space_id: 2,
+    user_id: 2
   )
   Booking.create(
-    date: "2023-03-20",
-    available: false
+    date: "2023-01-01",
+    available: false,
+    space_id: 3,
+    user_id: 3
   )
 end
